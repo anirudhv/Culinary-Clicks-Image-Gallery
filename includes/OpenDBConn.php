ae78265730f71cb1af3ba638a2ddea2f84363e06
anirudhv <anirudhv@purdue.edu>
2019-12-16 21:52:43 -0800

    Update OpenDBConn.php

--- includes/OpenDBConn.php
+++ includes/OpenDBConn.php
@@ -2,7 +2,7 @@
 $db_host = "165.22.191.102"; //set database host variable to server address
 $db_name = "project02venkataramanan"; //set database name variable to the name database username
 $db_username = "anirudhv"; //set database username variable to username used to login to phpMyAdmin
-$db_password = "Shivani1"; //ust database password variable to password used to login to phpMyAdmin
+$db_password = "password"; //ust database password variable to password used to login to phpMyAdmin
 
 $conn = new mysqli($db_host, $db_username, $db_password, $db_name); //set conn variable to a new mysqli variable which takes the previously declared local variables as paramaters. 
 
